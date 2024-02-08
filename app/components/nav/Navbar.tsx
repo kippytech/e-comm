3cbe1acb1c5824809e89a321bf36bef7684c63ff
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-08 07:59:17 +0300

    home page

---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Container from "../Container";
+import Link from "next/link";
+import { Redressed } from "next/font/google";
+//import CartCount from './CartCount'
+//import UserMenu from './UserMenu'
+//import { getCurrentUser } from '@/actions/getCurrentUser'
+import Categories from "./Categories";
+//import SearchBar from './SearchBar'
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
+
+async function Navbar() {
+  //const currentUser =  await getCurrentUser()
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-md">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex justify-between items-center gap-3 md:gap-0">
+            <Link
+              className={`${redressed.className} font-bold text-2xl`}
+              href="/"
+            >
+              SokoMall
+            </Link>
+            <div className="hidden md:block">{/* <SearchBar /> */}</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              {/* <CartCount />
+                      <UserMenu currentUser={currentUser} />*/}
+            </div>
+          </div>
+        </Container>
+      </div>
+      <Categories />
+    </div>
+  );
+}
+
+export default Navbar;
